d7d8111589cd3f6773eb9b4a21c403c3411b9d4d
MexTermin <astronboil.p@hotmail.com>
2022-03-27 18:39:31 -0400

    Feature: added base tree class

---
+++ Arboles/BaseTree.cs
@@ -0,0 +1,16 @@
+﻿namespace EstructurasDeDatos.Arboles
+{
+    abstract class BaseTree<T>
+    {
+        T root { get; set; }
+
+        public BaseTree(T root)
+        {
+            this.root = root;
+        }
+
+        public abstract T IsLeaf();
+        public abstract T DeGree();
+        public abstract T Height();
+    }
+}
